c9ade6e36fdb12e98ff01eb325225220f5a665b1
devauxjerome <90784691+devauxjerome@users.noreply.github.com>
2023-05-14 17:08:15 +0200

    correct zombie.h

--- zombie.h
+++ zombie.h
@@ -3,11 +3,15 @@
 
 
 /**
-PRE: sockfdController : un pointeur void qui pointe vers un entier représentant le descripteur de fichier de socket.
-POST: Les entrées/sorties standard (STDIN_FILENO, STDOUT_FILENO, STDERR_FILENO) sont redirigées vers le descripteur de fichier de socket fourni en entrée.
+* processus fils qui va s'occuper de lancer un terminal et renvoyer les reponses via le socket en parametre
+* PRE: sockfdController : un pointeur void qui pointe vers un entier représentant le descripteur de fichier de socket.
+* POST: Les entrées/sorties standard (STDIN_FILENO, STDOUT_FILENO, STDERR_FILENO) sont redirigées vers le descripteur de fichier de socket fourni en entrée.
 */
 void zombieFils(void* sockfdController);
 
-void zombie(char* portRecu);
+/**
+* fermeture du programme zombie et des ses processus fils
+*/
+void endZombieLabo(int sig);
 
 #endif
